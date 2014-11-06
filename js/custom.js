12ef74db7b8c332b93528fe5f13a5264ebff2319
Kevin McCarthy <kjmagic13@gmail.com>
2014-11-06 13:40:54 -0500

    contact form, is human checkbox

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,15 @@
 $(function() {
+
+	// contact form, is human?
+	$("#isHuman").change(function(){
+		if(this.checked) {
+			$('#contactSubmit').removeClass("disabled");
+		} else {
+			$('#contactSubmit').addClass("disabled");
+		}
+	});
+
+	// sticky footer
 	var docHeight, footerHeight, footerTop;
 	docHeight = $(window).height();
 	footerHeight = $("#page-footer").height();
@@ -6,4 +17,5 @@ $(function() {
 	if (footerTop < docHeight) {
 		return $("#page-footer").css("margin-top", (docHeight - footerTop) + "px");
 	}
+
 });
